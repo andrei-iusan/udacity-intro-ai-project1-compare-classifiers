4eb917616b0a9abb40492f37dde6c7be283f6ba3
Andrei Iusan <andrei.iusan.ai@googlemail.com>
2026-01-18 20:06:23 +0200

    calculate result statistics

--- intropyproject-classify-pet-images/calculates_results_stats.py
+++ intropyproject-classify-pet-images/calculates_results_stats.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/calculates_results_stats.py
 #                                                                             
-# PROGRAMMER:
-# DATE CREATED:                                  
+# PROGRAMMER: Andrei Iusan
+# DATE CREATED: 2026-01-18                                  
 # REVISED DATE: 
 # PURPOSE: Create a function calculates_results_stats that calculates the 
 #          statistics of the results of the programrun using the classifier's model 
@@ -68,6 +68,36 @@ def calculates_results_stats(results_dic):
                      and the classroom Item XX Calculating Results for details
                      on how to calculate the counts and statistics.
     """        
-    # Replace None with the results_stats_dic dictionary that you created with 
-    # this function 
-    return None
+    results_stats_dic = dict()
+    results_stats_dic['n_images'] = len(results_dic)
+    for key in results_dic:
+        if results_dic[key][2] == 1:
+            results_stats_dic['n_match'] = results_stats_dic.get('n_match', 0) + 1  
+        if results_dic[key][3] == 1:
+            results_stats_dic['n_dogs_img'] = results_stats_dic.get('n_dogs_img', 0) + 1
+        else:
+            results_stats_dic['n_notdogs_img'] = results_stats_dic.get('n_notdogs_img', 0) + 1
+        if results_dic[key][3] == 1 and results_dic[key][4] == 1:
+            results_stats_dic['n_correct_dogs'] = results_stats_dic.get('n_correct_dogs', 0) + 1
+        if results_dic[key][3] == 0 and results_dic[key][4] == 0:
+            results_stats_dic['n_correct_notdogs'] = results_stats_dic.get('n_correct_notdogs', 0) + 1
+        if results_dic[key][3] == 1 and results_dic[key][4] == 1 and results_dic[key][2] == 1:
+            results_stats_dic['n_correct_breed'] = results_stats_dic.get('n_correct_breed', 0) + 1
+        
+        
+    # Calculate percentages
+    if len(results_dic) > 0:
+        pct_match = (results_stats_dic['n_match'] / len(results_dic)) * 100
+        pct_correct_dogs = (results_stats_dic['n_correct_dogs'] / len(results_dic)) * 100
+        pct_correct_breed = (results_stats_dic['n_correct_breed'] / len(results_dic)) * 100
+        pct_correct_notdogs = (results_stats_dic['n_correct_notdogs'] / len(results_dic)) * 100
+    else:
+        pct_match, pct_correct_dogs, pct_correct_breed, pct_correct_notdogs = [0.0]*4
+
+    # Add percentages to the dictionary
+    results_stats_dic['pct_match'] = pct_match
+    results_stats_dic['pct_correct_dogs'] = pct_correct_dogs
+    results_stats_dic['pct_correct_breed'] = pct_correct_breed
+    results_stats_dic['pct_correct_notdogs'] = pct_correct_notdogs
+
+    return results_stats_dic
